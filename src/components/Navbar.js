b747708b7a216a3ae4155290522c724a8179c36e
CHANDRABABU <2100269@cognizant.com>
2023-06-13 17:22:37 +0530

    removing the unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,26 +30,8 @@ const Navbar = (props) => {
           <li className="nav-item">
             <a className={`nav-link text-${props.color==='black'?'dark':'light'}`} href="/about">{props.about}</a>
           </li>
-          {/* <li className="nav-item dropdown">
-            <a className={`nav-link dropdown-toggle text-${props.color==='black'?'dark':'light'}`} href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Products
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="/">Men</a></li>
-              <li><a className="dropdown-item" href="/">Women</a></li>
-              <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href="/">Kids</a></li>
-            </ul>
-          </li> */}
-          {/* <li className="nav-item">
-            <a className={`nav-link text-${props.color==='black'?'dark':'light'}`} href="/">SHOP</a>
-          </li> */}
         </ul>
         <form className="d-flex" role="search">
-        {/* <div className="form-check form-switch">
-          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Switch to {props.mode} Mode</label>
-        </div> */}
           <div className="bg-selector">
             <button type="button" onClick={props.backGroundSelector} className="selector" value="black" id='black'></button>
             <button type="button" onClick={props.backGroundSelector} className="selector" value="white" id='white'></button>
