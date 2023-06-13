56ec18521f06e641a570c15ebfac72b4c32e884a
CHANDRABABU <2100269@cognizant.com>
2023-06-13 17:53:58 +0530

    reverting the changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import {Outlet, Link} from "react-router-dom";
+import {Outlet, Link} from "react-router-dom";
 
 
 const propTypes = {title: PropTypes.string.isRequired,
@@ -18,20 +18,38 @@ const Navbar = (props) => {
     <>
     <nav className={`navbar navbar-expand-lg bg-${props.color==='black'?'light':'dark'}`} >
     <div className="container-fluid">
-      <a className={`navbar-brand text-${props.color==='black'?'dark':'light'}`} href="home">{props.title}</a>
+      <a className={`navbar-brand text-${props.color==='black'?'dark':'light'}`} href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className={`nav-link text-${props.color==='black'?'dark':'light'}`} aria-current="page" href="home">Home</a>
+            <Link className={`nav-link text-${props.color==='black'?'dark':'light'}`} aria-current="page" to="home">Home</Link>
           </li>
           <li className="nav-item">
-            <a className={`nav-link text-${props.color==='black'?'dark':'light'}`} href="/about">{props.about}</a>
+            <Link className={`nav-link text-${props.color==='black'?'dark':'light'}`} to="about">{props.about}</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <a className={`nav-link dropdown-toggle text-${props.color==='black'?'dark':'light'}`} href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Products
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="/">Men</a></li>
+              <li><a className="dropdown-item" href="/">Women</a></li>
+              <li><hr className="dropdown-divider"/></li>
+              <li><a className="dropdown-item" href="/">Kids</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className={`nav-link text-${props.color==='black'?'dark':'light'}`} href="/">SHOP</a>
           </li>
         </ul>
         <form className="d-flex" role="search">
+        {/* <div className="form-check form-switch">
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Switch to {props.mode} Mode</label>
+        </div> */}
           <div className="bg-selector">
             <button type="button" onClick={props.backGroundSelector} className="selector" value="black" id='black'></button>
             <button type="button" onClick={props.backGroundSelector} className="selector" value="white" id='white'></button>
@@ -42,6 +60,7 @@ const Navbar = (props) => {
       </div>
     </div>
   </nav>
+  <Outlet/>
   </>)
 }
 
