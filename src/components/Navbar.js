1a218c5bbb43e88956da4af40d7ea7815c18d732
CHANDRABABU <2100269@cognizant.com>
2023-06-13 16:35:58 +0530

    Bugfix/pagetitle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = (props) => {
     <>
     <nav className={`navbar navbar-expand-lg bg-${props.color==='black'?'light':'dark'}`} >
     <div className="container-fluid">
-      <a className={`navbar-brand text-${props.color==='black'?'dark':'light'}`} href="/">{props.title}</a>
+      <a className={`navbar-brand text-${props.color==='black'?'dark':'light'}`} href="/home">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
